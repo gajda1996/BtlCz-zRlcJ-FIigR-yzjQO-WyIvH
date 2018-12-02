15bd6c3bc42ddfe35d065595a417021d9635a377
gajda1996 <GajdosikM@seznam.cz>
2018-12-02 22:57:39 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php
-
-	if (!isset($_SESSION))
-	{
-		session_start();
-	}
-
-	$timelimit=600;
-	if (isset($_SESSION['timeout'])) 
-	{
-		$sessionTTL = time() - $_SESSION['timeout'];
-		if ($sessionTTL > $timelimit) 
-		{
-			session_destroy();
-			echo "logout";
-		}
-    }	
-	$_SESSION['timeout'] = time();
-
-
 	include "initdb.php";
-	include "login.php";
-	include "edit.php";
 	
 	$query_file = 'x.txt';
     $cislo=0;
