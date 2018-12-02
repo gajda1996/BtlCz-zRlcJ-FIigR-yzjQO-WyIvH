fc69e466a30e9e7d049eee9dac00917a19221a21
gajda1996 <GajdosikM@seznam.cz>
2018-12-02 17:38:16 +0100

    Add files via upload

--- html/reg2.php
+++ html/reg2.php
@@ -17,7 +17,7 @@ include 'menu.php';
 	{
 		$_POST["adduzivatel"] = 1;
 		include '../edit.php';
-		if ($_SESSION["error"] == 1)
+		if (isset($_SESSION["error"]))
 		{
 			echo "Uživatel s tímto loginem již existuje. Registrace byla neúspěšná.";
 		}
@@ -35,21 +35,6 @@ include 'menu.php';
 		echo "Hesla se neshodují";
 	}
  ?>
-
- <form action="reg2.php" method="POST">
-   <label for="name">Login</t></label>
-   <input type="text" name="login"<?php echo "value=\"" . $_POST["login"] . "\""?>>
-   <br>
-   <label for="surname">Heslo</t></label>
-   <input type="password" name="heslo">
-   <br>
-   <label for="surname">Potvrzení hesla</t></label>
-   <input type="password" name="heslo2">
-   <br>
-   <input type="submit" value="Registrovat">
-   </div>
- </form>
-
 </article>
 </body>
 </html>
