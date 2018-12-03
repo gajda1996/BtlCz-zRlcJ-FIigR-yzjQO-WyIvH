0cee39d7f5c37dcb567be04b26c7103f4e8f40eb
gajda1996 <GajdosikM@seznam.cz>
2018-12-03 19:04:02 +0100

    Add files via upload

--- html/profil.php
+++ html/profil.php
@@ -1,4 +1,4 @@
- <!DOCTYPE html>
+﻿ <!DOCTYPE html>
 <html lang="en">
 <head>
 <?php
@@ -12,10 +12,52 @@ include 'menu.php';
 ?>
 
 <article>
+<form action="upgradeuctu.php" method="POST">
+		<input type="submit" value="Staňte se sládkem">
+		</div>
+</form> 
+<br>
+<br>
 <?php
 	if (isset($_SESSION['login']) && isset($_SESSION['heslo']))
 	{
-	
+		if (isset($_POST['stareheslo']))
+		{
+			if ($_SESSION['heslo']!=$_POST['stareheslo'])
+			{
+				echo "Špatné stávající heslo";
+			}
+			else
+			{
+				if ($_POST['potvrdheslo']!=$_POST['heslo'])
+				{
+					echo "Nové heslo a jeho potvrzení se neshodují";
+				}
+				else
+				{
+					$_POST['updateuzivatel']=1;
+					$_POST['login']=$_SESSION['login'];
+					include '../edit.php';
+					$_SESSION['heslo']=$_POST['login'];
+					echo "Heslo bylo úspěšně změněno";
+				}
+			}
+		}
+	?>
+		<form action="profil.php" method="POST">
+		<label for="stareheslo">Původní heslo</t></label>
+		<input type="password" name="stareheslo">
+		<br>
+		<label for="heslo"></t>Nové heslo</label>
+		<input type="password" name="heslo">
+		<br>
+		<label for="potvrdheslo">Potvrzení nového hesla</t></label>
+		<input type="password" name="potvrdheslo">
+		<br>
+		<input type="submit" value="Změna hesla">
+		</div>
+	  </form>
+	<?php
 	}
 	else
 	{
---
+++ html/upgradeuctu.php
@@ -0,0 +1,58 @@
+﻿ <!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+include 'head.php';
+?>
+</head>
+<body>
+
+<?php
+include 'menu.php';
+?>
+
+<article>
+<?php
+if (isset($_POST['upgrade']))
+{
+	$_POST['addsladek']=1;
+	$_POST['login']=$_SESSION['login'];
+	include '../edit.php';
+	$_SESSION['typ']="sladek";
+	echo "Typ vašeho učetu byl změněn na sládka";
+}
+else if($_SESSION['typ']=="sladek")
+{
+	echo "Vy už jste sládkem";
+}
+else
+{
+?>
+<form action="upgradeuctu.php" method="POST">
+          <label for="jmeno">Jméno</label>
+          <input type="text" name="jmeno">
+          <br>
+          <label for="prijmeni">Příjmení</label>
+          <input type="text" name="prijmeni">
+          <br>
+          <label for="adr_mesto">Město</label>
+          <input type="text" name="adr_mesto">
+          <br>
+          <label for="adr_ulice">Ulice</label>
+          <input type="text" name="adr_ulice">
+          <br>
+          <label for="adr_cislo_domu">Číslo domu</label>
+          <input type="number" name="adr_cislo_domu">
+          <br>
+          <label for="PSC">PSČ</label>
+          <input type="number" name="PSC">
+          <br>
+          <input type="submit" name="upgrade" value="Změnit typ účtu">
+          </div>
+        </form>
+</article>
+<?php
+}
+?>
+</body>
+</html>
